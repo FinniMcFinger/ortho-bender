e5e333f56e44e84540f65ab1c661795ff084cabb
FinniMcFinger <rrmcgrew@gmail.com>
2026-02-23 14:09:33 -0600

    connects to channels

--- main.py
+++ main.py
@@ -7,11 +7,37 @@ import calendar as cal
 import discord
 import logging
 
-from discord.ext import commands
+from discord.ext import commands, tasks
 
 
-GUILD = const.GUILD
 log = logging.getLogger('discord')
 intents = discord.Intents.default()
-client = commands.Bot(command_prefix='!', intents=intents)
-client.run(const.TOKEN)
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+
+@bot.event
+async def on_ready():
+    config = (f'token: {const.TOKEN}; guild: {const.GUILD}; old cal channel: {const.OLD_CAL_CHANNEL}, '
+              f'new cal channel: {const.NEW_CAL_CHANNEL}')
+    print(config)
+    await post_readings.start()
+
+
+@tasks.loop(seconds=1.0)
+async def post_readings():
+    print('sending messages')
+    old_cal_channel = await bot.fetch_channel(const.OLD_CAL_CHANNEL)
+    print(f'Got old cal channel: {old_cal_channel}')
+    new_cal_channel = await bot.fetch_channel(const.NEW_CAL_CHANNEL)
+    print(f'Got new cal channel: {new_cal_channel}')
+    exit()
+
+
+@post_readings.before_loop
+async def before():
+    await bot.wait_until_ready()
+    print(f'{bot.user} is ready.')
+
+
+
+bot.run(const.TOKEN)

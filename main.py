867f5153dd0607161c85534ed2951cf2b9190847
FinniMcFinger <rrmcgrew@gmail.com>
2026-02-23 15:36:21 -0600

    runs as aiocron.crontab cron job

--- main.py
+++ main.py
@@ -3,41 +3,58 @@ import dotenv
 dotenv.load_dotenv()
 
 import constants as const
-import calendar as cal
+import cal
+import posts
+
+import aiocron
 import discord
 import logging
 
-from discord.ext import commands, tasks
+from discord.ext import commands
 
 
-log = logging.getLogger('discord')
-intents = discord.Intents.default()
-bot = commands.Bot(command_prefix='!', intents=intents)
+class CronJobs:
+    def __init__(self, bot: commands.Bot) -> None:
+        log = logging.getLogger('CronJobs')
+
+        @aiocron.crontab(const.CRON_TIME)
+        async def post_readings():
+            log.debug('sending messages')
+            old_cal_channel = await bot.fetch_channel(const.OLD_CAL_CHANNEL)
+            log.debug(f'Got old cal channel: {old_cal_channel}')
+            new_cal_channel = await bot.fetch_channel(const.NEW_CAL_CHANNEL)
+            log.debug(f'Got new cal channel: {new_cal_channel}')
+            old_cal_data = cal.get_today_data(use_new_cal=False)
+            old_cal_content = cal.get_post_contents(old_cal_data)
+            old_cal_messages = posts.create_messages(old_cal_content)
+            new_cal_data = cal.get_today_data()
+            new_cal_content = cal.get_post_contents(new_cal_data)
+            new_cal_messages = posts.create_messages(new_cal_content)
+
+            for message in old_cal_messages:
+                await old_cal_channel.send(message)
+
+            for message in new_cal_messages:
+                await new_cal_channel.send(message)
 
 
-@bot.event
-async def on_ready():
-    config = (f'token set?: {const.TOKEN is not None}; guild: {const.GUILD}; old cal channel: {const.OLD_CAL_CHANNEL}, '
-              f'new cal channel: {const.NEW_CAL_CHANNEL}')
-    log.info(config)
-    await post_readings.start()
+class Bender(commands.Bot):
+    def __init__(self):
+        super().__init__(command_prefix='!', help_command=None, intents=discord.Intents.default())
+        self.log = logging.getLogger('Bender')
+
+    async def setup_hook(self) -> None:
+        cron = CronJobs(self)
+
+    async def close(self):
+        await super().close()
+
+    async def on_ready(self) -> None:
+        config = (
+            f'token set?: {const.TOKEN is not None}; guild: {const.GUILD}; old cal channel: {const.OLD_CAL_CHANNEL}, '
+            f'new cal channel: {const.NEW_CAL_CHANNEL}')
+        self.log.info(config)
 
 
-@tasks.loop(seconds=1.0)
-async def post_readings():
-    log.debug('sending messages')
-    old_cal_channel = await bot.fetch_channel(const.OLD_CAL_CHANNEL)
-    log.debug(f'Got old cal channel: {old_cal_channel}')
-    new_cal_channel = await bot.fetch_channel(const.NEW_CAL_CHANNEL)
-    log.debug(f'Got new cal channel: {new_cal_channel}')
-    exit()
-
-
-@post_readings.before_loop
-async def before():
-    await bot.wait_until_ready()
-    log.info(f'{bot.user} is ready.')
-
-
-
-bot.run(const.TOKEN)
+bender = Bender()
+bender.run(const.TOKEN)
---
+++ posts.py
@@ -0,0 +1,35 @@
+import logging
+
+
+log = logging.getLogger(__name__)
+
+
+def create_messages(content: dict) -> list:
+    messages = []
+    log.debug(f'content: {content}')
+
+    # initial day post
+    header = content.get('day', '')
+    feasts = content.get('feasts', '')
+    commemorations = content.get('commemorations', '')
+    readings_summary = content.get('readings', {}).get('summary', '')
+    messages.append(f'{header}\n{feasts}\n{commemorations}\n{readings_summary}')
+
+    # readings {summary: string, passages: [{heading: string, verses: [string]}]
+    passages = content['readings']['passages']
+    current_message = ''
+
+    for passage in passages:
+        current_message += passage['heading']
+        for verse in passage['verses']:
+            if len(current_message) + len(verse) > 2000:
+                messages.append(current_message);
+                current_message = '> '
+
+            current_message += verse
+
+        if len(current_message) > 0:
+            messages.append(current_message)
+            current_message = ''
+
+    return messages
